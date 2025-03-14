b31f77335d8e1b27f786e1ece007d42e8bea4937
Ahmed0427 <ahmedabdallah1090@gmail.com>
2025-03-14 11:34:48 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# diff.py
+
+A file diff tool that compares two files and highlights the differences,
+showing added, removed, or modified content. 
+
+### Usage
+
+```bash
+./diff.py <file1> <file2>
+```
---
+++ diff.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+
+import sys
+
+def read_file(path):
+    with open(path) as f:
+        return list(filter(lambda line: line != "", f.read().splitlines()))
+
+ADD = 'A'
+SUBST = 'S'
+IGNORE = 'I'
+REMOVE = 'R'
+
+def lev_dist_actions(lines1, lines2):
+    n, m = len(lines1), len(lines2)
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    actions = [['X'] * (m + 1) for _ in range(n + 1)]
+
+    dp[0][0] = 0
+    actions[0][0] = IGNORE
+
+    for i in range(1, n + 1):
+        dp[i][0] = i
+        actions[i][0] = REMOVE
+
+    for j in range(1, m + 1):
+        dp[0][j] = j
+        actions[0][j] = ADD
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if lines1[i - 1] == lines2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+                actions[i][j] = IGNORE
+            else:
+
+                dp[i][j] = dp[i - 1][j];
+                actions[i][j] = REMOVE
+
+                if dp[i][j] > dp[i][j - 1]:
+                    dp[i][j] = dp[i][j - 1]
+                    actions[i][j] = ADD
+
+
+                if dp[i][j] > dp[i - 1][j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1];
+                    actions[i][j] = SUBST
+
+                dp[i][j] += 1
+
+
+    trace = []
+    i, j = n, m
+    while i and j:
+        action = actions[i][j]
+        if action == ADD:
+            j -= 1
+            trace.append((action, lines2[j]))
+        elif action == REMOVE:
+            i -= 1
+            trace.append((action, lines1[i]))
+        elif action == SUBST:
+            i -= 1
+            j -= 1
+            trace.append((action, lines1[i], lines2[j]))
+        elif action == IGNORE:
+            i -= 1
+            j -= 1
+            trace.append((action, lines1[i]))
+        else:
+            assert False, "Unreachable"
+
+    while i:
+        action = actions[i][j]
+        i -= 1
+        trace.append((action, lines1[i]))
+
+    while j:
+        action = actions[i][j]
+        j -= 1
+        trace.append((action, lines2[j]))
+
+    return trace
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print(f"Usage: {sys.argv[0]} <file1> <file2>")
+        exit(1)
+
+    lines1 = read_file(sys.argv[1])
+    lines2 = read_file(sys.argv[2])
+
+    for action in lev_dist_actions(lines1, lines2):
+        if action[0] == 'I':
+            print('   ' + action[1])
+        elif action[0] == 'A':
+            print('++ ' + action[1])
+        elif action[0] == 'R':
+            print('-- ' + action[1])
+        elif action[0] == 'S':
+            print('-- ' + action[1])
+            print('++ ' + action[2])
+        else:
+            assert False, "Unreachable"
