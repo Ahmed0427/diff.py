8d94b7a57897e0e0500f77d79399f8d9ab2b5429
Ahmed0427 <ahmedabdallah1090@gmail.com>
2025-06-01 00:57:52 +0300

    better coloring

--- diff.py
+++ diff.py
@@ -98,11 +98,11 @@ if __name__ == '__main__':
         if action[0] == 'I':
             print(' ' + action[1])
         elif action[0] == 'A':
-            print('\033[36m' + '+' + action[1] + '\033[0m')
+            print('\033[32;1m' + '+' + action[1] + '\033[0m')
         elif action[0] == 'R':
-            print('\033[31m' + '-' + action[1] + '\033[0m')
+            print('\033[31;1m' + '-' + action[1] + '\033[0m')
         elif action[0] == 'S':
-            print('\033[31m' + '-' + action[1] + '\033[0m')
-            print('\033[36m' + '+' + action[2] + '\033[0m')
+            print('\033[31;1m' + '-' + action[1] + '\033[0m')
+            print('\033[32;1m' + '+' + action[2] + '\033[0m')
         else:
             assert False, "Unreachable"
